524e9b2e85bd14c38f1850c0415db49f20218c5f
Savinay Kumar <iamsavinaykumar@gmail.com>
2021-05-29 13:42:12 +0530

    installed mongo deps

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const morgan = require('morgan');
 const router = require('./routes');
 
 const app = express();
-const logger = morgan('dev');
+const logger = morgan('dev'); // sets morgan logger mode: dev, combined
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('view engine', 'ejs'); // sets view engine to EJS
+app.set('views', 'views'); // sets view folder location
 
 app.use(logger);
-app.use(express.json());
+app.use(express.json()); // use express json bodyparser
 app.use(express.urlencoded({
   extended: true,
-}));
+})); // use express urlencoded body parser
 
 app.use(router);
 
